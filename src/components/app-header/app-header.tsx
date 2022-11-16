f0cd8baabe948fe4b9c2c67611382a0ffd49ab83
alex <admin@elasticweb.ru>
2022-11-16 21:54:39 +0300

    fix nav

--- src/components/app-header/app-header.tsx
+++ src/components/app-header/app-header.tsx
@@ -7,25 +7,26 @@ import {
 import styles from './styles.module.css'
 
 export const AppHeader = () => {
-    const navItemClass = `${styles.nav_item} p-5`
-    
+
     return (
         <header>
             <ul className={styles.nav}>
-                <li className={navItemClass}>
+                <li className={`${styles.nav_item} ${styles.logo} pt-5`}>
+                    <Logo />
+                </li>
+                <li className={`${styles.nav_item} ${styles.constructor} pt-5`}>
                     <BurgerIcon type='primary' />
                     <span className='pl-2 text text_type_main-default'>
                         Конструктор
                     </span>
                 </li>
-                <li className={navItemClass}>
+                <li className={`${styles.nav_item} ${styles.order_list} pt-5`}>
                     <ListIcon type='secondary' />
                     <span className='pl-2 text text_type_main-default text_color_inactive'>
                         Лента заказов
                     </span>
                 </li>
-                <Logo />
-                <li className={navItemClass}>
+                <li className={`${styles.nav_item} ${styles.profile} pt-5`}>
                     <ProfileIcon type='secondary' />
                     <span className='pl-2 text text_type_main-default text_color_inactive'>
                         Личный кабинет
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -11,4 +11,4 @@ export interface Ingredient {
     image_mobile: string,
     image_large: string,
     __v: number
- }
+}
